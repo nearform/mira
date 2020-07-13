2d9b09f936beb49b600d4a76ff55802ad015d191
Thiago Kroger <thiagokroger@gmail.com>
2020-07-13 09:09:36 -0300

    fix(tests): Skipped cicd tests (#17)
    * fix(tests): Skipped cicd tests (#17)
    
    * fix(tests): Skipped cicd tests - remove tryGetContext (#17)

--- src/cdk/constructs/cicd/index.test.ts
+++ src/cdk/constructs/cicd/index.test.ts
@@ -7,23 +7,9 @@ jest.mock('config')
 interface Context {
   [name: string]: string | any[] | any
 }
-describe.skip('CICD', () => {
+describe('CICD', () => {
   it('Creates a CICD stack with role as Caller Identity', async () => {
     const app = new App()
-    app.node.tryGetContext = (): Context => ({
-      webAppUrl: 'test.local',
-      gitHubTokenSecretArn: '123',
-      repositoryProvider: 'codecommit',
-      codeCommitUserPublicKey: 'rsa xxx',
-      accounts: [
-        'Test'
-      ],
-      env: {
-        account: '692283872684',
-        region: 'eu-west-1'
-      },
-      profile: 'ci-profile'
-    })
     const cicd = new Cicd(app, {
       callerIdentityResponse: {
         Arn: 'arn:aws:sts::123456789012:assumed-role/demo/TestAR'
@@ -37,20 +23,6 @@ describe.skip('CICD', () => {
 
   it('Creates a CICD stack with user as Caller Identity', () => {
     const app = new App()
-    app.node.tryGetContext = (): Context => ({
-      webAppUrl: 'test.local',
-      gitHubTokenSecretArn: '123',
-      repositoryProvider: 'codecommit',
-      codeCommitUserPublicKey: 'rsa xxx',
-      accounts: [
-        'Test'
-      ],
-      env: {
-        account: '692283872684',
-        region: 'eu-west-1'
-      },
-      profile: 'ci-profile'
-    })
     const cicd = new Cicd(app, {
       callerIdentityResponse: {
         Arn: 'arn:aws:iam::101259067028:user/demo'
