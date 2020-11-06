0afb5f552033ac7df582cc2b55c27eb7ecd4a245
Davide Fiorello <davide@codeflyer.com>
2020-11-06 17:38:49 +0100

    fix region, add utils

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ require('source-map-support').install()
 
 export * from './cdk/stack'
 export * from './cdk/app'
+export * from './cdk/utils'
 export * from './cdk/constructs'
 // export * from './config/job-config'
 export * from './config/utils'
